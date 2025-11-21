40ad3f04c64fb3726ea6dd8d7d80a2cf6a65655b
Felix Nordén <felixnorden@gmail.com>
2025-11-21 09:57:25 +0000

    fix: build issue from GH actions

--- packages/playground/src/app/layout.tsx
+++ packages/playground/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -28,11 +29,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <PlaygroundProvider>
-          {children}
-        </PlaygroundProvider>
+        <PlaygroundProvider>{children}</PlaygroundProvider>
       </body>
     </html>
   );
 }
-
